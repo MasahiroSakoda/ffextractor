4cc72dee135282bb24139ddaf3ff2bca72fccff3
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-16 05:12:29 +0900

    fix: assert error checking

--- internal/util/file_test.go
+++ internal/util/file_test.go
@@ -114,7 +114,7 @@ func TestRemoveFile(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if _, errCreate := CreateFile(tt.filepath); errCreate != nil {
-				t.Errorf("CreateFile() error = %v, want %v", errCreate, tt.wantErr)
+				assert.Equal(t, tt.wantErr, errCreate != nil)
 			}
 			if errRemove := RemoveFile(tt.filepath); errRemove != nil {
 				assert.Equal(t, tt.wantErr, errRemove != nil)
