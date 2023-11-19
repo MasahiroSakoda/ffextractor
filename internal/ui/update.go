b159c7df20b0ca18a17deaed084dd903a5632aea
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-19 20:44:44 +0900

    feat(bubbletea): update table with detected segments

--- internal/ui/update.go
+++ internal/ui/update.go
@@ -6,29 +6,47 @@ import (
 )
 
 const (
-	padding  = 2
+	padding  = 1
 	maxWidth = 80
 )
 
 // Update : bubletea required method
 func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmd    tea.Cmd
+		cmds []tea.Cmd
+	)
+
+	m.table, cmd = m.table.Update(msg)
+	cmds = append(cmds, cmd)
+
 	switch msg := msg.(type) {
-	// spinner
 	case spinner.TickMsg:
-		var cmd tea.Cmd
-		m.Spinner, cmd = m.Spinner.Update(msg)
-		return m, cmd
+		switch {
+		case m.loading:
+			m.spinner, cmd = m.spinner.Update(msg)
+			cmds = append(cmds, cmd)
+		}
+	case silenceDetectedMsg:
+		m.loading = false
+		m.table.SetRows(msg.rows)
+
+	case blackoutDetectedMsg:
+		m.loading = false
+
 	case tea.WindowSizeMsg:
+		m.termWidth, m.termHeight = msg.Width, msg.Height
 		return m, nil
+	case tickMsg:
+		tickEvery()
 	// key
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q", "esc":
 			return m, tea.Quit
-		default:
-			return m, nil
 		}
 	default:
-		return m, nil
+		return m, tea.Batch(cmds...)
 	}
+	return m, tea.Batch(cmds...)
 }
