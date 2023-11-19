314f5badb0bd4633d6d9ddc81b4729fa78d9e643
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-19 20:46:03 +0900

    feat(bubbletea): define view to display table

--- internal/ui/view.go
+++ internal/ui/view.go
@@ -1,13 +1,19 @@
 package ui
 
 import (
+	"fmt"
 	"strings"
 )
 
 // View : bubbletea required method
 func (m *Model) View() string {
-	pad := strings.Repeat(" ", padding)
+	pad := strings.Repeat(" ", padding * 5)
+	var loadingMessage = ""
+	if m.loading {
+		loadingMessage = baseStyle(fmt.Sprintf("%s Detecting silence segments...", m.spinner.View()))
+	}
 	return "\n" +
-		pad + m.Spinner.View() +
-		pad + helpStyle("Press any key to quit")
+		loadingMessage + "\n" +
+		m.table.View() + "\n" +
+		pad + helpStyle("Press any key to quit") + "\n"
 }
