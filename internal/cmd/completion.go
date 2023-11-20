4409aa84c6a56e3bc082bf282154e9e400ed19e6
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-21 04:25:20 +0900

    refactor(cmd): extract anonymous function for `ffextractor completion`

--- internal/cmd/completion.go
+++ internal/cmd/completion.go
@@ -3,77 +3,65 @@ package cmd
 import (
 	"os"
 
-	"github.com/MasahiroSakoda/ffextractor/internal/constants"
-
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	rootCmd.AddCommand(newCompletionCmd())
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generates shell completion scripts",
+	Long:  "Generates shell completion scripts",
+	Args:  cobra.MinimumNArgs(1),
+	RunE:  runCompletionCmd,
 }
 
-func newCompletionCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "completion",
-		Short: "Generates shell completion scripts",
-		Long:  "Generates shell completion scripts",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			err := cmd.Help()
-			if err != nil {
-				return err
-			}
-			return constants.ErrInvalidParam
-		},
-	}
-	cmd.AddCommand(
-		newCompletionBashCmd(),
-		newCompletionZshCmd(),
-		newCompletionFishCmd(),
-	)
-	return cmd
+var completionBashCmd = &cobra.Command {
+	Use:   "bash",
+	Short: "Generates bash completion scripts",
+	Long:  "Generates bash completion scripts",
+	RunE:  runCompletionBashCmd,
 }
 
-func newCompletionBashCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "bash",
-		Short: "Generates bash completion scripts",
-		Long:  "Generates bash completion scripts",
-		Run: func(cmd *cobra.Command, args []string) {
-			err := rootCmd.GenBashCompletion(os.Stdout)
-			if err != nil {
-				os.Exit(1)
-			}
-		},
-	}
-	return cmd
+var completionZshCmd = &cobra.Command{
+	Use:   "zsh",
+	Short: "Generates zsh completion scripts",
+	Long:  "Generates zsh completion scripts",
+	RunE:   runCompletionZshCmd,
 }
 
-func newCompletionZshCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "zsh",
-		Short: "Generates zsh completion scripts",
-		Long:  "Generates zsh completion scripts",
-		Run: func(cmd *cobra.Command, args []string) {
-			err := rootCmd.GenZshCompletion(os.Stdout)
-			if err != nil {
-				os.Exit(1)
-			}
-		},
-	}
-	return cmd
+var completionFishCmd = &cobra.Command{
+	Use:   "fish",
+	Short: "Generates fish completion scripts",
+	Long:  "Generates fish completion scripts",
+	RunE:  runCompletionFishCmd,
 }
 
-func newCompletionFishCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "fish",
-		Short: "Generates fish completion scripts",
-		Long:  "Generates fish completion scripts",
-		Run: func(cmd *cobra.Command, args []string) {
-			err := rootCmd.GenFishCompletion(os.Stdout, true)
-			if err != nil {
-				os.Exit(1)
-			}
-		},
+func runCompletionCmd(cmd *cobra.Command, _ []string) error {
+	return cmd.Help()
+}
+
+func runCompletionBashCmd(cmd *cobra.Command, _ []string) error {
+	err := cmd.GenBashCompletion(os.Stdout)
+	if err != nil {
+		os.Exit(1)
+		return err
 	}
-	return cmd
+	return nil
+}
+
+func runCompletionZshCmd(cmd *cobra.Command, _ []string) error {
+	err := cmd.GenZshCompletion(os.Stdout)
+	if err != nil {
+		os.Exit(1)
+		return err
+	}
+	return nil
+}
+
+func runCompletionFishCmd(cmd *cobra.Command, _ []string) error {
+	err := cmd.GenFishCompletion(os.Stdout, true)
+	if err != nil {
+		os.Exit(1)
+		return err
+	}
+	return nil
 }
