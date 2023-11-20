4cb04c10a75c345ee090c3c5f9190af4b9534817
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-21 00:03:42 +0900

    refactor: add comment into constant variables

--- internal/constants/errors.go
+++ internal/constants/errors.go
@@ -3,34 +3,50 @@ package constants
 import "errors"
 
 var (
-	// ErrUnexpected : unexpected error
+	// ErrUnexpected is used for unknown reason
 	ErrUnexpected = errors.New("unexpected error")
 
-	// Extraction
+	// ErrSilenceDetect is error for detect silence segments whilte using ffmpeg
 	ErrSilenceDetect  = errors.New("silence detection error")
+	// ErrBlackoutDetect is error for detect blackout segments whilte using ffmpeg
 	ErrBlackoutDetect = errors.New("blackout detection error")
-	ErrSplitSegment   = errors.New("split processing error")
+	// ErrParseSegment is error for parse segments
 	ErrParseSegment   = errors.New("parse processing error")
+	// ErrSplitSegment is error for split segments
+	ErrSplitSegment   = errors.New("split processing error")
 
-	// Config
+	// ErrLoadConfig is error for loading config file
 	ErrLoadConfig       = errors.New("failed to load config file")
+	// ErrParseConfig is error for parsing config file
 	ErrParseConfig      = errors.New("failed to parse env to config struct")
+	// ErrInvalidConfigKey is error for invalid key into config
 	ErrInvalidConfigKey = errors.New("invalid config key")
 
-	// ErrFileOpen : error for opening file
+	// ErrFileOpen is error while opening file
 	ErrFileOpen     = errors.New("failed to open file")
+	// ErrFileRead is error while reading file
 	ErrFileRead     = errors.New("failed to read file")
+	// ErrFileWrite is error while writing file
 	ErrFileWrite    = errors.New("failed to write file")
+	// ErrFileRemove is error while removing file
 	ErrFileRemove   = errors.New("failed to remove file or directory")
+	// ErrFileNotFound is error file existence
 	ErrFileNotFound = errors.New("no such file or directory")
 
+	// ErrMkdir is error while making directory
 	ErrMkdir      = errors.New("failed to create directory")
+	// ErrFileScan is error while scanning file or directory
 	ErrFileScan   = errors.New("failed to scan file or directory")
 
+	// ErrJSONEncode is error while encoding JSON
 	ErrJSONEncode = errors.New("failed to encode json")
+	// ErrJSONDecode is error while decoding JSON
 	ErrJSONDecode = errors.New("failed to decode json")
-	ErrTomlEncode = errors.New("failed to encode json")
-	ErrTomlDecode = errors.New("failed to decode json")
+	// ErrTomlEncode is error while encoding TOML
+	ErrTomlEncode = errors.New("failed to encode toml")
+	// ErrTomlDecode is error while decoding TOML
+	ErrTomlDecode = errors.New("failed to decode toml")
 
+	// ErrInvalidParam is error for wrong command line parameters
 	ErrInvalidParam = errors.New("invalid parameter")
 )
