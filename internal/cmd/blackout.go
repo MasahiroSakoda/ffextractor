f40568d0819dbc737c955a59f9424132fc9d7e3f
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-21 03:29:36 +0900

    refactor(cmd): extract anonymous function for `ffextractor blackout` command

--- internal/cmd/blackout.go
+++ internal/cmd/blackout.go
@@ -1,6 +1,12 @@
 package cmd
 
 import (
+	"os"
+
+	"github.com/MasahiroSakoda/ffextractor/internal/constants"
+	"github.com/MasahiroSakoda/ffextractor/internal/util"
+
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -9,7 +15,23 @@ var blackoutCmd = &cobra.Command{
 	Short: "Extract video exclude blackout parts.",
 	Long:  "Extract video exclude blackout parts.",
 	Args:  cobra.MinimumNArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil
-	},
+	RunE: extractExcludeBlackout,
+}
+
+func extractExcludeBlackout(_ *cobra.Command, args []string) error {
+	var path = ""
+	contains, err := util.ContainsMedia(args[0])
+	if err != nil {
+		return constants.ErrInvalidParam
+	}
+
+	if contains {
+		path = args[0]
+	} else {
+		os.Exit(1)
+		return constants.ErrInvalidParam
+	}
+	logrus.Debugf("%s", path)
+
+	return nil
 }
