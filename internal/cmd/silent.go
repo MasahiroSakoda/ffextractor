7e6aa692741c54b829ed92ca79846b4efc17ac74
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-21 03:28:40 +0900

    refactor(cmd): extract anonymous function for `ffextractor silent` command

--- internal/cmd/silent.go
+++ internal/cmd/silent.go
@@ -4,12 +4,12 @@ import (
 	"os"
 
 	"github.com/MasahiroSakoda/ffextractor/internal/constants"
-	"github.com/MasahiroSakoda/ffextractor/internal/util"
 	"github.com/MasahiroSakoda/ffextractor/internal/ui"
+	"github.com/MasahiroSakoda/ffextractor/internal/util"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	tea "github.com/charmbracelet/bubbletea"
 )
 
 var silentCmd = &cobra.Command{
@@ -17,36 +17,30 @@ var silentCmd = &cobra.Command{
 	Short: "Extract media exclude silent parts.",
 	Long:  "Extract media exclude silent parts.",
 	Args:  cobra.MinimumNArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		path := ""
-		contains, err := util.ContainsMedia(args[0])
-		if err != nil {
-			logrus.Errorf("%s: %v", constants.ErrFileNotFound, err)
-			return err
-		}
-		if contains {
-			path = args[0]
-		} else {
-			logrus.Errorf("%s: %v", constants.ErrInvalidParam, err)
-			os.Exit(1)
-		}
-		// segments, err := ffmpeg.DetectSilence(path)
-		// if err != nil {
-		// 	return err
-		// }
-		// jsonData, err := json.Marshal(segments)
-		// if err != nil {
-		// 	return err
-		// }
-		// fmt.Printf("%s", string(jsonData))
+	RunE:  extractExcludeSilence,
+}
 
-		m := ui.New(path)
-		p := tea.NewProgram(m)
-		if _, err := p.Run(); err != nil {
-			logrus.Errorf("%s: %v", constants.ErrUnexpected, err)
-			os.Exit(1)
-		}
-		p.Quit()
-		return nil
-	},
+func extractExcludeSilence(_ *cobra.Command, args []string) error {
+	var path = ""
+	contains, err := util.ContainsMedia(args[0])
+	if err != nil {
+		return constants.ErrInvalidParam
+	}
+
+	if contains {
+		path = args[0]
+	} else {
+		os.Exit(1)
+		return constants.ErrInvalidParam
+	}
+
+	m := ui.New(path)
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		logrus.Errorf("%s: %v", constants.ErrUnexpected, err)
+		os.Exit(1)
+	}
+	p.Quit()
+
+	return nil
 }
