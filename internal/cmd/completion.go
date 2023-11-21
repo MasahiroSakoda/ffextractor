c601030a2c33a566124a885641e40a8db52ff1a6
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-21 20:36:11 +0900

    refactor(cmd): improve complete command to run efficiently
    remove sub commands

--- internal/cmd/completion.go
+++ internal/cmd/completion.go
@@ -1,67 +1,26 @@
 package cmd
 
 import (
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
-var completionCmd = &cobra.Command{
-	Use:   "completion",
-	Short: "Generates shell completion scripts",
-	Long:  "Generates shell completion scripts",
-	Args:  cobra.MinimumNArgs(1),
-	RunE:  runCompletionCmd,
-}
-
-var completionBashCmd = &cobra.Command {
-	Use:   "bash",
-	Short: "Generates bash completion scripts",
-	Long:  "Generates bash completion scripts",
-	RunE:  runCompletionBashCmd,
-}
-
-var completionZshCmd = &cobra.Command{
-	Use:   "zsh",
-	Short: "Generates zsh completion scripts",
-	Long:  "Generates zsh completion scripts",
-	RunE:   runCompletionZshCmd,
-}
-
-var completionFishCmd = &cobra.Command{
-	Use:   "fish",
-	Short: "Generates fish completion scripts",
-	Long:  "Generates fish completion scripts",
-	RunE:  runCompletionFishCmd,
-}
-
-func runCompletionCmd(cmd *cobra.Command, _ []string) error {
-	return cmd.Help()
-}
-
-func runCompletionBashCmd(cmd *cobra.Command, _ []string) error {
-	err := cmd.GenBashCompletion(os.Stdout)
-	if err != nil {
-		os.Exit(1)
-		return err
+func newCompletionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "completion",
+		Short: "Generates shell completion scripts",
+		Long:  "Generates shell completion scripts",
+		Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+		ValidArgs: []string{"bash", "zsh", "fish"},
+		RunE:  func(cmd *cobra.Command, args []string) error {
+			switch args[0] {
+			case "bash":
+				_ = cmd.GenBashCompletion(cmd.OutOrStderr())
+			case "zsh":
+				_ = cmd.GenZshCompletion(cmd.OutOrStderr())
+			case "fish":
+				_ = cmd.GenFishCompletion(cmd.OutOrStderr(), true)
+			}
+			return nil
+		},
 	}
-	return nil
-}
-
-func runCompletionZshCmd(cmd *cobra.Command, _ []string) error {
-	err := cmd.GenZshCompletion(os.Stdout)
-	if err != nil {
-		os.Exit(1)
-		return err
-	}
-	return nil
-}
-
-func runCompletionFishCmd(cmd *cobra.Command, _ []string) error {
-	err := cmd.GenFishCompletion(os.Stdout, true)
-	if err != nil {
-		os.Exit(1)
-		return err
-	}
-	return nil
 }
---
+++ internal/cmd/completion_test.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestCompletionCmd(t *testing.T) {
+	tests := []struct{
+		name    string
+		param   string
+		output  string
+		wantErr bool
+	}{
+		{ param: "bash", output: "bash completion for bash",           wantErr: false, name: "return bash completion " },
+		{ param: "zsh",  output: "zsh completion for zsh",             wantErr: false, name: "return zsh completion" },
+		{ param: "fish", output: "fish completion for fish",           wantErr: false, name: "return fish completion" },
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := newCompletionCmd()
+			cmd.SetArgs([]string{tt.param})
+			b := bytes.NewBufferString("")
+			cmd.SetOut(b)
+			err := cmd.Execute()
+			if err != nil {
+				t.Fatalf("newCompletionCmd() execute error: %v", err)
+			}
+			o, err := io.ReadAll(b)
+			if err != nil {
+				t.Fatalf("stdout capturing error: %v", err)
+			}
+			var output = string(o)
+			if output == "" {
+				t.Errorf("Unexpected output: %s", output)
+			}
+		})
+	}
+}
