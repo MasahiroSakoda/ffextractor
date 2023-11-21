860ee89d0b243deb350026855495a08d4060ed98
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-22 00:26:18 +0900

    refactor(cmd): improve `blackout` command to run efficiently
    & add test

--- internal/cmd/blackout.go
+++ internal/cmd/blackout.go
@@ -10,28 +10,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var blackoutCmd = &cobra.Command{
-	Use:   "blackout",
-	Short: "Extract video exclude blackout parts.",
-	Long:  "Extract video exclude blackout parts.",
-	Args:  cobra.MinimumNArgs(1),
-	RunE: extractExcludeBlackout,
-}
+func newBlackoutCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "blackout",
+		Short: "Extract video exclude blackout parts.",
+		Long:  "Extract video exclude blackout parts.",
+		Args:  cobra.MinimumNArgs(1),
+		RunE: func(_ *cobra.Command, args []string) error {
+			var path = ""
+			contains, err := util.ContainsMedia(args[0])
+			if err != nil {
+				return constants.ErrInvalidParam
+			}
 
-func extractExcludeBlackout(_ *cobra.Command, args []string) error {
-	var path = ""
-	contains, err := util.ContainsMedia(args[0])
-	if err != nil {
-		return constants.ErrInvalidParam
+			if contains {
+				path = args[0]
+			} else {
+				os.Exit(1)
+				return constants.ErrInvalidParam
+			}
+			logrus.Debugf("%s", path)
+
+			return nil
+		},
 	}
-
-	if contains {
-		path = args[0]
-	} else {
-		os.Exit(1)
-		return constants.ErrInvalidParam
-	}
-	logrus.Debugf("%s", path)
-
-	return nil
 }
