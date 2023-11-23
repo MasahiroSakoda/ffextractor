14ea4ddedbc06e607e0b4e14cf7e74f858f15c33
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-23 21:11:37 +0900

    test(detect): remove test

--- internal/extractor/detect_test.go
+++ internal/extractor/detect_test.go
@@ -1,4 +1,4 @@
-package ffmpeg
+package extractor
 
 import (
 	"testing"
@@ -24,30 +24,6 @@ func TestDetectSilence(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := DetectSilence(testDataPath(tt.file))
-			if err != nil {
-				assert.Error(t, err)
-			} else {
-				assert.Equal(t, tt.wantErr, got != nil)
-			}
-		})
-	}
-}
-
-func TestDetectBlackout(t *testing.T) {
-	tests := []struct {
-		file    string
-		name    string
-		wantErr bool
-	}{
-		// TODO: Uncomment after implement DetectBlackout()
-		// { file: testDataPath("invalid.mp4"),  wantErr: true, name: "Return error with invalid file"},
-		// { file: testDataPath("blackout.mp4"), wantErr: true, name: "Undetected with blackout only video"},
-		// { file: testDataPath("whiteout.mp4"), wantErr: true, name: "Undetected with whiteout only video"},
-		// { file: testDataPath("mixed.mp4"),    wantErr: false, name: "Detected blackout parts with proper file"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := DetectBlackout(testDataPath(tt.file))
 			if err != nil {
 				assert.Error(t, err)
 			} else {
