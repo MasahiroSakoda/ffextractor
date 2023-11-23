11284c49a4975556ace2771c3be8226327eaf4dc
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-23 17:53:13 +0900

    refactor: replace segment model

--- internal/ffmpeg/detect.go
+++ internal/ffmpeg/detect.go
@@ -3,15 +3,18 @@ package ffmpeg
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
 
-	"github.com/MasahiroSakoda/ffextractor/internal/util"
-	"github.com/MasahiroSakoda/ffextractor/internal/model"
 	"github.com/MasahiroSakoda/ffextractor/internal/config"
+	"github.com/MasahiroSakoda/ffextractor/internal/constants"
+	"github.com/MasahiroSakoda/ffextractor/internal/segment"
+	"github.com/MasahiroSakoda/ffextractor/internal/util"
 )
 
 // DetectSilence returns stdout for detected silence parts
-func DetectSilence(src string) (segments []model.AudioSegment, err error) {
+func DetectSilence(src string) (segments []segment.Model, err error) {
 	extract := config.Root.Extract
 	var threshold = extract.Threshold
 	var duration  = extract.SilenceDuration
@@ -21,41 +24,45 @@ func DetectSilence(src string) (segments []model.AudioSegment, err error) {
 		"-f", "null", "-"}
 	output, err := exec.Command("ffmpeg", args...).CombinedOutput()
 	if err != nil {
-		return []model.AudioSegment{}, err
+		return []segment.Model{}, err
 	}
 	return parseDetectedSegments(src, output)
-
 }
 
-func parseDetectedSegments(src string, data []byte) (segments []model.AudioSegment, err error) {
-	var segment model.AudioSegment
-	segment.Start = 0
+func parseDetectedSegments(src string, data []byte) (segments []segment.Model, err error) {
+	var s segment.Model
+	s.Start = 0
 	correction   := 0.0
 	for _, line := range strings.Split(string(data), "\n") {
 		if strings.Contains(line, "silence_end") {
 			seconds, err := util.ConvertToSeconds(util.GetStringInBetween(line, "silence_end: ", " "))
 			if err == nil {
-				segment.End = seconds + correction
-				segment.Filename = src
-				segment.Duration = segment.End - segment.Start
-				if segment.Duration > 0.25 {
-					segments = append(segments, segment)
+				s.End = seconds + correction
+				s.Input = src
+				s.Duration = s.End - s.Start
+				if s.Duration > 0.25 {
+					segments = append(segments, s)
 				}
-				segment.Start = seconds + correction
+				s.Start  = seconds + correction
+				s.Output = fmt.Sprintf("%s_%s%s",
+					constants.CommandName,
+					strconv.FormatFloat(s.Start, 'f', -1, 64),
+					filepath.Ext(s.Input),
+				)
 			} else {
 				fmt.Printf("%s", err)
 			}
 		} else if strings.Contains(line, "time=") {
 			seconds, err := util.ConvertToSeconds(util.GetStringInBetween(line, "time=", " "))
 			if err == nil {
-				segment.End = seconds
-				segment.Duration = segment.End - segment.Start
-				segment.Filename = src
-				if segment.Duration < 0.25 && len(segments) > 0 {
+				s.End = seconds
+				s.Duration = s.End - s.Start
+				s.Input = src
+				if s.Duration < 0.25 && len(segments) > 0 {
 					segments[len(segments)-1].End = seconds
 					segments[len(segments)-1].Duration = segments[len(segments)-1].End - segments[len(segments)-1].Start
 				} else {
-					segments = append(segments, segment)
+					segments = append(segments, s)
 
 				}
 			} else {
@@ -63,12 +70,12 @@ func parseDetectedSegments(src string, data []byte) (segments []model.AudioSegme
 			}
 		}
 	}
-	newSegments := make([]model.AudioSegment, len(segments))
+	newSegments := make([]segment.Model, len(segments))
 	i := 0
 
-	for _, segment := range segments {
-		if segment.Duration > 0.1 {
-			newSegments[i] = segment
+	for _, seg := range segments {
+		if seg.Duration > 0.1 {
+			newSegments[i] = seg
 			i++
 		}
 	}
@@ -82,7 +89,7 @@ func parseDetectedSegments(src string, data []byte) (segments []model.AudioSegme
 }
 
 // DetectBlackout returns stdout for detected blackout parts
-func DetectBlackout(src string) (segments []model.VideoSegment, err error) {
+func DetectBlackout(src string) (segments []segment.Model, err error) {
 	duration  := config.Root.Extract.BlackoutDuration
 	args := []string{
 		"-i", src,
@@ -94,7 +101,7 @@ func DetectBlackout(src string) (segments []model.VideoSegment, err error) {
 	}
 	fmt.Printf("%s", string(output))
 
-	var segment model.VideoSegment
+	var segment segment.Model
 	segment.Start = 0
 
 	// TODO: collect detected parts
