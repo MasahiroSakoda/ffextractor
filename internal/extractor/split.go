ed8478b5491f087ec6e27c5737c5a898139ad35b
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-11-24 11:30:45 +0900

    refactor(extractor): rewrite split process using `u2takey/ffmpeg-go`

--- internal/extractor/split.go
+++ internal/extractor/split.go
@@ -1,36 +1,38 @@
 package extractor
 
 import (
-	"fmt"
 	"path/filepath"
-	"strconv"
 
 	"github.com/MasahiroSakoda/ffextractor/internal/config"
 	"github.com/MasahiroSakoda/ffextractor/internal/segment"
 
-	// ffmeg "github.com/u2takey/ffmpeg-go"
+	fg "github.com/u2takey/ffmpeg-go"
 )
 
 // SplitDetectedSegment returns error to extract media segment exclude detected parts
 func SplitDetectedSegment(segment segment.Model, tempDir string) error {
 	output := filepath.Join(tempDir, segment.Output)
 	encode := config.Root.Encode.SplitWithEncode
+	// fmt.Println(output)
 
-	args := []string {
-		"-y",
-		"-ss", strconv.FormatFloat(segment.Start, 'f', -1, 64),
-		"-i",  segment.Input,
-		"-t",  strconv.FormatFloat(segment.Duration, 'f', -1, 64),
+	a, err := fg.Probe(segment.Output)
+	if err != nil { return err }
+
+	totalDuration, err := probeDuration(a)
+	if err != nil { return err }
+
+	var outArgs fg.KwArgs
+	if !encode {
+		outArgs = fg.KwArgs{"t": segment.Duration}
+	} else {
+		outArgs = fg.KwArgs{"t": segment.Duration, "c": "copy"}
 	}
-	if encode {
-		args = append(args, "-c", "copy")
-	}
-	args = append(args, output)
-	fmt.Println(args)
-	// output, err := exec.Command("ffmpeg", args...).CombinedOutput()
-	// if err != nil {
-	// 	return err
-	// }
-	// fmt.Println(string(output))
-	return nil
+
+	err = fg.Input(segment.Input, fg.KwArgs{"ss": segment.Start}).
+			Output(output, outArgs).
+			OverWriteOutput().
+			GlobalArgs("-progress", "unix://" + TempSock(totalDuration)).
+			Compile().
+			Run()
+	return err
 }
