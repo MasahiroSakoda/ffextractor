4434e7531f4ddc6f15142ac4104c845435da6f59
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-12-14 12:20:51 +0900

    feat(ui): add update condition for detect/split/concat

--- internal/ui/update.go
+++ internal/ui/update.go
@@ -3,18 +3,17 @@ package ui
 
 import (
 	"os"
-	"fmt"
 	"strconv"
-	"time"
 
 	"github.com/MasahiroSakoda/ffextractor/internal/constants"
-	"github.com/MasahiroSakoda/ffextractor/internal/extractor"
 	"github.com/MasahiroSakoda/ffextractor/internal/segment"
 	"github.com/MasahiroSakoda/ffextractor/internal/util"
 
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/cockroachdb/errors"
+	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -39,8 +38,8 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		tickEvery()
 
 	case errMsg:
-		if msg.err != nil {
-			fmt.Println(msg.err)
+		errors.WithDetail(msg.err, "Error detected")
+		if !m.detecting && !m.splitting && !m.concatenating {
 			return m, tea.Quit
 		}
 
@@ -55,77 +54,73 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 		}
 
+	case tea.QuitMsg:
+		m.removeTempFiles(m.tempDir)
+
 	// spinner
 	case spinner.TickMsg:
 		switch {
-		case m.loading:
+		case m.detecting || m.splitting || m.concatenating:
 			m.spinner, cmd = m.spinner.Update(msg)
 			cmds = append(cmds, cmd)
 		}
 
 	// after silence detected
 	case silenceDetectedMsg:
-		m.loading = false
-		m.makeTableRows(msg.segments)
-
-		// create temp directory
-		dirPrefix := constants.CommandName + "_"
-		tempDir, err := os.MkdirTemp("", dirPrefix)
-		errorDetected(err)
-		defer func() {
-			os.RemoveAll(tempDir)
-		}()
-
-		for i, segment := range msg.segments {
-			m.table.SetCursor(i)
-			err := extractor.SplitDetectedSegment(segment, tempDir)
-			errorDetected(err)
-		}
-		m.table.Blur()
-		return m, tea.Quit
+		m.detecting = false
+		m.splitting = true
+		m.makeTableRows()
+		cmd := m.fetchSplittedSegments()
+		cmds = append(cmds, cmd)
 
 	// msg for split detected segments
 	case splitProcessingMsg:
+		// m.segments = m.removeSegment(msg.index)
+		// m.makeTableRows()
+
+	// msg for split process completed
+	case splitCompletedMsg:
+		m.splitting = false
+		m.concatenating = true
+		m.concatFile.Close()
+		// cmd := m.executeConcatSegments(m.path)
+		// cmds = append(cmds, cmd)
+
+	// msg for concatenate process completed
+	case concatCompletedMsg:
+		m.concatenating = false
+		// m.removeTempFiles(m.tempDir)
+		logrus.Debugf("Concatenate completed: %s", msg.output)
+		// return m, tea.Quit
 	}
 
 	return m, tea.Batch(cmds...)
 }
 
-// tickEvery : update every time
-func tickEvery() tea.Cmd {
-	return tea.Tick(time.Millisecond, func(t time.Time) tea.Msg {
-		return tickMsg(t)
-	})
-}
-
-func errorDetected(e error) tea.Msg {
-	if e != nil {
-		return errMsg{err: e}
+func (m *Model) removeTempFiles(path string) {
+	err := os.RemoveAll(path)
+	if err != nil {
+		logrus.Errorf("%s: %v", constants.ErrFileRemove, err)
 	}
-	return nil
 }
 
 // makeTableRows makes table using detected segments
-func (m *Model) makeTableRows(segments []segment.Model) {
+func (m *Model) makeTableRows() {
 	var rows []table.Row
-	for i, segment := range segments {
+	for i, segment := range m.segments {
 		var row = table.Row{
 			strconv.Itoa(i + 1),
 			util.GetFilenameFromPath(segment.Input),
-			strconv.FormatFloat(segment.Start,    'f', -1, 64),
-			strconv.FormatFloat(segment.End,      'f', -1, 64),
-			strconv.FormatFloat(segment.Duration, 'f', -1, 64),
+			util.SecondsToString(segment.Start),
+			util.SecondsToString(segment.End),
+			util.SecondsToString(segment.Duration),
 		}
 		rows = append(rows, row)
 	}
 	m.table.SetRows(rows)
 }
 
-// fetchSilenceSegments return message to detect silence segments
-func (m *Model) fetchSilenceSegments() tea.Msg {
-	segments, err := extractor.DetectSilentSegments(m.path)
-	if err != nil {
-		return errMsg{err: constants.ErrSilenceDetect}
-	}
-	return silenceDetectedMsg{segments: segments}
+// removeSegment returns removed segments with index
+func (m *Model) removeSegment(index int) []segment.Model {
+	return m.segments[:index+copy(m.segments[index:], m.segments[index+1:])]
 }
