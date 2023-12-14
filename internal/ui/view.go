0bb6e3ac1a38af37be9f12e64ee8d3cd4cb8b389
MasahiroSakoda <thunderbird.m.s@gmail.com>
2023-12-14 12:19:11 +0900

    feat(ui): add loading message for detect/split/concat

--- internal/ui/view.go
+++ internal/ui/view.go
@@ -4,17 +4,26 @@ package ui
 import (
 	"fmt"
 	"strings"
+
 )
 
 // View : bubbletea required method
 func (m *Model) View() string {
 	pad := strings.Repeat(" ", padding * 5)
 	var loadingMessage = ""
-	if m.loading {
+	// var helpMessage = ""
+
+	switch {
+	case m.detecting:
 		loadingMessage = baseStyle(fmt.Sprintf("%s Detecting silence segments...", m.spinner.View()))
+	case m.splitting:
+		loadingMessage = baseStyle(fmt.Sprintf("%s Splitting silence segments...", m.spinner.View()))
+	case m.concatenating:
+		loadingMessage = baseStyle(fmt.Sprintf("%s Concatenating splitted segments...", m.spinner.View()))
 	}
 	return "\n" +
 		loadingMessage + "\n" +
 		m.table.View() + "\n" +
+		"\n" +
 		pad + helpStyle("Press \"esc\" or \"q\" key to quit") + "\n"
 }
